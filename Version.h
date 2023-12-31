d491f3442891e882259259a8920bb6a84a1a3e91
Christopher Ng <facboy@gmail.com>
2023-12-31 12:07:25 +0000

    manually set version

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 0,0,0,0
+#define FILEVER 4,5,20,3804
 #define PRODUCTVER FILEVER
-#define STRFILEVER "0.0.0.0"
+#define STRFILEVER "4.5.20.3804"
 #define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"0.0.0"
+#define APPVERSION L"4.5.20"
 #define RAINMETER_VERSION ((0 * 1000000) + (0 * 1000) + 0)
-#define STRCOPYRIGHT "0000 Rainmeter Team"
-const int revision_number = 0;
+#define STRCOPYRIGHT "2024 Rainmeter Team"
+const int revision_number = 3804;
